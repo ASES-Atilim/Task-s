9e6c2820fa09d9e54a7e60bd764c5333abcdaecc
Brakselk <151445645+Brakselk@users.noreply.github.com>
2023-12-12 16:52:43 +0300

    Update Task1.cpp

--- Task_1_2_1/BurakSelek/Task1.cpp
+++ Task_1_2_1/BurakSelek/Task1.cpp
@@ -12,7 +12,7 @@ void getValue(float *ptra,float *ptrb,float *ptrc){			//Input function
 	scanf("%f",ptrc);
 }
 
-float calculator(float a,float b,float c){			//Calculate function
+float calculator(float a,float b,float c){				//Calculate function
 	
 	float tempResult,result;
 	
@@ -22,7 +22,8 @@ float calculator(float a,float b,float c){			//Calculate function
 	return result;
 }
 //ekstra tempResult değişkenine neden gerek duydun? (yanlış olduğu için değil açıklamanı istiyorum)
-//
+/*Kodu ilk yazdığımda sonucu bastıramıyordum, işlemlerden dolayı mı yoksa return bölümü mü yanlış olduğunu anlayamamıştım 
+  düzeltmeyi denerken de en son bu şekilde çalıştığını görünce değiştirmedim.*/
 
 int main(){
 	
@@ -30,13 +31,16 @@ int main(){
 	getValue(&a,&b,&c);
 	result=calculator(a,b,c);
 	
-	
 	printf("Result of the calculation is: %.3f \n",result);
 	//.3f ne işe yarıyor?
-	//
+	//Sonucu bastırırken .'dan sonra 3 basamak olması için yazdım.
 
 	//bu satırda bir şeyler eksik
+	return 0;
 }
 
 // değerleri alırken neden böyle bir yol izledin?
+// Alternatif bir yol aklıma gelmedi ben de en basit ve bildiğim şekilde aldım değerleri.
 // bu kodu normal bir insanın anlayacağı şekilde açıklayabilir misin?
+/* getValue Fonksiyonu ile girdi alan, calculator Fonksiyonu ile verilen formüle göre hesap yapan,
+main Fonksiyonunda da her iki fonksiyonu kullanıp kullanıcıdan değer alan ve hesaplama yapıp hesaplama sonucunu ekrana bastıran bir kod. */
