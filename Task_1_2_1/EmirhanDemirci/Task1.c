06054c1d3d79f3435f4aba5009029604da77d24d
Emirhan Demirci <139490942+emiirhandemirci@users.noreply.github.com>
2023-12-13 21:37:30 +0300

    Update Task1.c

--- Task_1_2_1/EmirhanDemirci/Task1.c
+++ Task_1_2_1/EmirhanDemirci/Task1.c
@@ -4,7 +4,7 @@ double function(double a,double b,double c){
 	return 4*a*c/(a+b);
 }
 //fonksiyon ismi anlamlı ve fonksiyonun amacına hitab etmeli
-
+//haklısın buna dikkat edicem.
 int main (void){
 	
 	double a,b,c;
@@ -23,7 +23,7 @@ int main (void){
 		scanf("%lf",&b);
 	}
 	//böyle bir while'a neden ihtiyaç duyduğunu açıklamanı istiyorum
-	//
+	//burada paydada 0 olduguğunda tanımsız olacağı için bu loopu kullandım böylece 0 olduğunda tekrar değer istiyor tabi bir error mesajı da yayınlayabilirmişim şuan fark ettim.
 
 	printf("enter c value : ");
 	scanf("%lf",&c);
@@ -34,10 +34,12 @@ int main (void){
 	//örnek
 	//result = function(a,b,c);
 	//printf("result = %lf",result);
+	//evet burda biraz kolaya kaçmışım daha sonra result'ı kullanıcak olsam patlıyorum :)
 	return (0);
 	
 }
 
 
 //bu kodu normal bir insanın anlayabileceği şekilde açıklayabilir misin?
-//
+//en başta kullanıcağım döngüleri ve kodları içeren kütüphaneyi çağırdım sonrasında da benden istenen fonksiyonu oluşturdum ardından gereken girdileri alıp işleme soktum, paydada 0 olduğu taktirde fonksiyonum tanımsız olacağı için bir döngü oluşturdum, en sonda bunun sonucunu verdim.
+//emeğin için teşekkürler Berk. Ben son günü pazar günü sanıyordum bir dahakine daha dikkatli olurum kabul ettiğin için de teşekkür ederim.
